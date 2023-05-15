64617e2a5b0afbe210de7e6be7e5d9ab6c4baddc
Wandering Consciousness <simon@funx.jp>
2023-05-15 12:31:46 +0800

    Shuffle the problems

--- app/src/main/java/com/example/immadisairaj/quiz/HomeActivity.java
+++ app/src/main/java/com/example/immadisairaj/quiz/HomeActivity.java
@@ -23,6 +23,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
